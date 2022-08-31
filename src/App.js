58ef8fc170ec5c3033412d8ae16852f00bd4a7c9
cseswapon <cseswapon@gmail.com>
2022-09-01 01:35:02 +0600

    [ADD] - added login oppups

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
+import "./App.css";
 // eslint-disable-next-line no-unused-vars
 import bootstrap from "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Task2 from './Components/Task2';
+import Task2 from "./Components/Task2";
+import Singup from "./Components/Singup/Singup";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
-      <Task2/>
-      
-      
+        {/* <Routes> */}
+          <Task2 />
+          {/* <Route path="/singUp" element={<Singup />} /> */}
+        {/* </Routes> */}
       </BrowserRouter>
-      
     </div>
   );
 }
---
+++ src/Components/Singup/Singup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Singup = () => {
+    return (
+        <div>
+            <h1>Sing Up From</h1>
+        </div>
+    );
+};
+
+export default Singup;
